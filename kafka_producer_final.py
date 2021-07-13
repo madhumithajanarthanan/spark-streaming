9ee3fc45c5786ba5e4335ccaaa3c9e35dc0c35a8
madhumithajanarthanan <63897211+madhumithajanarthanan@users.noreply.github.com>
2021-07-13 16:21:56 +0530

    Add files via upload

---
+++ kafka_producer_final.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import time
+from kafka import KafkaProducer
+
+# Set Kafka config
+kafka_broker_hostname='localhost'
+kafka_broker_portno='9092'
+kafka_broker=kafka_broker_hostname + ':' + kafka_broker_portno
+kafka_topic='air_quality'
+
+data_send_interval=5
+
+
+if __name__ == "__main__":
+    # Create KafkaProducer instance
+    producer = KafkaProducer(bootstrap_servers=kafka_broker)
+
+    # Load demo data
+    # air_1 = pd.read_csv('air1.csv')
+    air_2 = pd.read_csv('/opt/city_hour_final_new.csv')
+
+    # Send demo data to Kafka broker
+    for _index in range(0, len(air_2)):
+        json_air_2 = air_2[air_2.index==_index].to_json(orient='records')[1:-1]
+        producer.send(kafka_topic, bytes(json_air_2, 'utf-8'))
+        print(json_air_2)
+        time.sleep(data_send_interval)
